b3ff014b832bbc31ee33496c1e917e3601058243
Prabal Rawal <prabalrawal@Prabals-Laptop.local>
2024-04-25 12:20:03 +0530

    sa

--- mychat/settings.py
+++ mychat/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
-import dj_database_url
+
+django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,9 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-django_heroku.settings(locals())
+
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
